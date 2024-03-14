1e0790f850564b2b1dc7f966e76ff81153383ed1
RandomThacker <randomthacker@gmail.com>
2024-03-14 15:52:51 +0530

    feat: React Query Setup

---
+++ src/api/tasks.jsx
@@ -0,0 +1,4 @@
+export async function fetchTasks(){
+    const response = await fetch("http://localhost:3000/tasks")
+    return response.json()
+}
--- src/components/ui/FormPost.tsx
+++ src/components/ui/FormPost.tsx
@@ -1,11 +1,13 @@
 "use client";
-
+import { useQuery } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FormInputPost } from "@/types";
 import { FC } from "react";
+
+import { fetchTasks } from "../../api/tasks";
 
 interface FormPostProps {
   submit: SubmitHandler<FormInputPost>;
@@ -14,25 +16,43 @@ interface FormPostProps {
 
 const FormPost: FC<FormPostProps> = ({ submit, isEditing }) => {
   const { register, handleSubmit } = useForm<FormInputPost>();
+  // fetch list
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTasks,
+  });
+  console.log(data);
+  
 
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
   return (
     <form
       onSubmit={handleSubmit(submit)}
       className="flex flex-col items-center justify-center gap-5 mt-10"
     >
       <Input
-        {...register("task",{ required: true})}
+        {...register("task", { required: true })}
         type="text"
         placeholder="Task"
         className="max-w-md w-full"
       />
       <Textarea
-        {...register("taskDesc",{ required: true})}
+        {...register("taskDesc", { required: true })}
         placeholder="Task Details"
         className="max-w-md w-full"
       />
-      <Button type="submit" variant="outline" className="bg-black text-white w-full max-w-md">
-        {isEditing? "Update" : "Add"}
+      <Button
+        type="submit"
+        variant="outline"
+        className="bg-black text-white w-full max-w-md"
+      >
+        {isEditing ? "Update" : "Add"}
       </Button>
     </form>
   );
