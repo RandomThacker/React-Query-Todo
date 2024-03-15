268ea5f2a0e88af22dabbb4563c436aa3044e566
RandomThacker <randomthacker@gmail.com>
2024-03-15 17:54:11 +0530

    fix: deployment bugs

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,18 +1,19 @@
+// src/app/create/page.tsx
 "use client"
 import { createTask } from "@/api/tasks";
-import BackButton from "@/components/BackButton";
 import FormPost from "@/components/FormPost";
 import { FormInputPost } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from 'next/navigation';
 import { SubmitHandler } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
+import React, { PropsWithChildren } from "react";
 
 interface CreatePageProps {
   onCloseDialog: () => void;
 }
 
-const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
+const CreatePage: React.FC<PropsWithChildren<CreatePageProps>> = ({ onCloseDialog }) => {
   const { push } = useRouter();
   const queryClient = useQueryClient();
 
@@ -36,7 +37,6 @@ const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
 
   return (
     <div>
-      {/* <BackButton/> */}
       <h1 className="text-2xl my-4 font-bold text-center">Add New Task</h1>
       <FormPost submit={handleCreateTask} isEditing={false}/>
     </div>
