3819ce81da1b325755f60655a8594d6ca0f4be45
RandomThacker <randomthacker@gmail.com>
2024-03-15 10:16:25 +0530

    style: UI modifications

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -13,60 +13,58 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { deleteTask, fetchTask, fetchTasks } from "@/api/tasks";
 import { SkeletonCard } from "./SkeletonCard";
-
+import { motion } from "framer-motion";
 
 interface InputProps {
-  taskId : string,
-  task: string,
-  taskDescp: string
+  taskId: string;
+  task: string;
+  reference: any;
 }
 
-const PostCard: FC<InputProps>=({ taskId, task, taskDescp })=> {
+const PostCard: FC<InputProps> = ({ taskId, task, reference }) => {
   // console.log(task, taskDescp, taskId);
   const queryClient = useQueryClient();
 
-
-  const {
-    data: tasks,
-  } = useQuery({
-    queryKey: ["todos",taskId],
-    queryFn: ()=>fetchTask(taskId),
+  const { data: todos } = useQuery({
+    queryKey: ["todos", taskId],
+    queryFn: () => fetchTask(taskId),
   });
-  // console.log(tasks);
+  console.log(todos);
 
   const deletePostMutation = useMutation({
     mutationFn: deleteTask,
-    onSuccess: () =>{
-      queryClient.invalidateQueries({queryKey: ['todos']})
-    }
-  })
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
 
-  const handleDelete =()=>{
+  const handleDelete = () => {
     console.log(taskId);
-    deletePostMutation.mutate(taskId)
-    
-  }
+    deletePostMutation.mutate(taskId);
+  };
 
   return (
-    <div>
-       {/* <Link href={`/task/${taskId}`}> */}
-      <Card className="shadow-xl border cursor-pointer">
-        <CardHeader>
-          <CardTitle>{task}</CardTitle>
-          <CardDescription>{taskDescp}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>{taskDescp}</p>
-        </CardContent>
-        <Link href={`/edit/${taskId}`} className="p-2 pb-12">
-        <Button>Edit</Button>
-      </Link>
-
-      <Button variant="destructive" onClick={handleDelete}>Delete</Button>
+    <motion.div drag dragConstraints={reference}>
+      <Card className="shadow-xl border cursor-pointer z-50 bg-zinc-900/90 border-none text-zinc-100 w-[250px] h-[200px] flex flex-col justify-between">
+        {/* <Link href={`/task/${taskId}`}> */}
+          <CardHeader>
+            <CardTitle>{task}</CardTitle>
+          </CardHeader>
+          <div className="flex item-center align-middle justify-center">
+            <Link
+              href={`/edit/${taskId}`}
+              className="bg-green-500 h-full w-[100%] flex align-middle justify-center items-center py-2 rounded-bl-lg"
+            >
+              <h1>Edit</h1>
+            </Link>
+            <div className="bg-purple-700 h-[100%] w-[100%]  flex align-middle justify-center items-center py-2 rounded-br-lg">
+              <h1 onClick={handleDelete}>Delete</h1>
+            </div>
+          </div>
+        {/* </Link> */}
       </Card>
-      {/* </Link> */}
-    </div>
+    </motion.div>
   );
-}
+};
 
 export default PostCard;
