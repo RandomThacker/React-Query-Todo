1bb596273fb0bc107902b1eac6f7c60f9f339f3b
RandomThacker <randomthacker@gmail.com>
2024-03-15 19:25:09 +0530

    fix: deployement bugs

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,43 +1,47 @@
-"use client"
+"use client";
 import { createTask } from "@/api/tasks";
+import BackButton from "@/components/BackButton";
 import FormPost from "@/components/FormPost";
 import { FormInputPost } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { SubmitHandler } from "react-hook-form";
-import { v4 as uuidv4 } from 'uuid';
-import React, { PropsWithChildren } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-interface CreatePageProps {
-  onCloseDialog: () => any;
-}
-
-const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
+function CreatePage() {
   const { push } = useRouter();
   const queryClient = useQueryClient();
 
   const createTaskMutation = useMutation({
     mutationFn: createTask,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['todos'] });
-      onCloseDialog(); 
-    }
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      // console.log("redirecting to new page");
+    },
   });
 
   const handleCreateTask: SubmitHandler<FormInputPost> = async (data) => {
     try {
-      const newTask = { id: uuidv4(), ...data };
+      // let newData = data.todoName
+      const newTask = { id: uuidv4(), ...data }; // Assuming data contains task details
       await createTaskMutation.mutateAsync(newTask);
-      push('/');
+      // console.log(newData);
+      // console.log(data);
+
+      push("/");
     } catch (error) {
-      console.error('Error creating task:', error);
+      console.error("Error creating task:", error);
+      // Handle error as needed, e.g., show error message to the user
     }
   };
 
   return (
-    <div>
-      <h1 className="text-2xl my-4 font-bold text-center">Add New Task</h1>
-      <FormPost submit={handleCreateTask} isEditing={false}/>
+    <div className="h-screen bg-zinc-800">
+      <BackButton />
+      <div className="bg-zinc-100 shadow-lg w-[550px] py-16 rounded-lg block m-auto">
+        <h1 className="text-2xl my-4 font-bold text-center">Add a new task</h1>
+        <FormPost submit={handleCreateTask} isEditing={false} />
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,18 @@
-// src/app/create/page.tsx
 "use client";
-import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import PostCard from "@/components/PostCard";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 import { useQuery } from "@tanstack/react-query";
-import { fetchData, fetchTasks } from "../api/tasks";
+import { fetchTasks } from "../api/tasks";
 import { SkeletonCard } from "@/components/SkeletonCard";
-import { useRef, useState } from "react";
-import CreatePage from "./create/page";
+import { useRef } from "react";
+ 
+const Home = () => {
 
-interface CreatePageProps {
-  onCloseDialog: () => void;
-}
-
-const Home: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
+  const ref = useRef(null)
+  
   const {
     isPending,
     isError,
@@ -34,23 +22,16 @@ const Home: React.FC = () => {
     queryKey: ["todos"],
     queryFn: fetchTasks,
   });
-
-  const { data } = useQuery({
-    queryKey: ["check"],
-    queryFn: fetchData,
-  });
-
-  let checkData = data?.data?.code;
-  console.log("fetch data", checkData);
+  console.log(todos);
 
   if (isPending) {
     return (
       <div className="bg-zinc-800">
-        <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mx-10">
-          <SkeletonCard />
-          <SkeletonCard />
-          <SkeletonCard />
-        </div>
+      <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mx-10">
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+      </div>
       </div>
     );
   }
@@ -59,85 +40,35 @@ const Home: React.FC = () => {
     return <span>Error: {error.message}</span>;
   }
 
-  const handleOpenDialog = () => {
-    setIsDialogOpen(true);
-  };
-
-  const handleCloseDialog = () => {
-    setIsDialogOpen(false);
-  };
-
   return (
-    <div className="bg-zinc-800 h-screen w-[100%] relative" ref={ref}>
-      <h1
-        className="text-zinc-900 text-[13vw] font-semibold absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-[0] select-none pointer-events-none"
-        style={{ zIndex: "0" }}
-      >
-        Todos
-      </h1>
-      <Dialog open={isDialogOpen}>
-        <DialogTrigger asChild>
-          <Button
-            variant="outline"
-            className="bg-white text-silver rounded-xl fixed bottom-10 right-10 py-10 px-7"
-            onClick={handleOpenDialog}
-          >
-            <h1 className="text-xl">
-              Add a<br /> new Task
-            </h1>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <CreatePage onCloseDialog={handleCloseDialog} />
-        </DialogContent>
-      </Dialog>
-      <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mx-10 z-[10]">
-        {checkData && checkData === 404 ? (
-          <span>No todos available.</span>
-        ) : (
-          todos.map(
-            (task: { _id: string; todoName: string; taskId: string }) => (
-              <PostCard
-                key={task._id}
-                todoName={task.todoName}
-                taskId={task._id}
-                reference={ref}
-              />
-            )
-          )
-        )}
-      </div>
+    <div className="bg-zinc-800 h-screen w-[100%] " ref={ref}>
+    <div  className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mx-10 z-[9999] absolute" style={{zIndex:"100"}} >
+    
+     {todos.map((task: {
+       _id: string; todoName: string; taskId: string 
+}) => (
+        <PostCard
+        key={task._id}
+        todoName={task.todoName} 
+        taskId={task._id} 
+      
+          reference = {ref}
+        />
+      ))}
+      
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline" className="rounded-full py-7 bg-[#ffcd10] fixed top-10 right-10">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-lightbulb"
-            >
-              <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
-              <path d="M9 18h6" />
-              <path d="M10 22h4" />
-            </svg>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Fun Tip</DialogTitle>
-            <DialogDescription>
-              You can hold and drag the todo cards.<br/> Play with them and have fun :D
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <Link href="/create">
+        <Button 
+          variant="outline"
+          className="bg-black text-white rounded-xl fixed bottom-10 right-10 h-20"
+        >
+          <PlusIcon className="h-7 w-7 mr-2" />
+          <h1 className="text-xl">Add a new Task</h1>
+        </Button>
+      </Link>
+    </div>
+    <h1 className="text-zinc-900 text-[13vw] font-semibold absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-0" style={{zIndex:"0"}}>Todos</h1>
+
     </div>
   );
 };
