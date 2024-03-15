9448a3444141d39cf03b7247e771cdc3e5eec5b9
RandomThacker <randomthacker@gmail.com>
2024-03-15 18:08:11 +0530

    feat: Added tip dialogue box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 // src/app/create/page.tsx
-"use client"
+"use client";
 import React from "react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import PostCard from "@/components/PostCard";
 import { useQuery } from "@tanstack/react-query";
@@ -18,7 +25,12 @@ const Home: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const { isPending, isError, data: todos, error } = useQuery({
+  const {
+    isPending,
+    isError,
+    data: todos,
+    error,
+  } = useQuery({
     queryKey: ["todos"],
     queryFn: fetchTasks,
   });
@@ -57,13 +69,22 @@ const Home: React.FC = () => {
 
   return (
     <div className="bg-zinc-800 h-screen w-[100%] relative" ref={ref}>
-      <h1 className="text-zinc-900 text-[13vw] font-semibold absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-[0] select-none pointer-events-none" style={{ zIndex: "0" }}>
+      <h1
+        className="text-zinc-900 text-[13vw] font-semibold absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-[0] select-none pointer-events-none"
+        style={{ zIndex: "0" }}
+      >
         Todos
       </h1>
       <Dialog open={isDialogOpen}>
         <DialogTrigger asChild>
-          <Button variant="outline" className="bg-white text-silver rounded-xl fixed bottom-10 right-10 py-10 px-7" onClick={handleOpenDialog}>
-            <h1 className="text-xl">Add a<br /> new Task</h1>
+          <Button
+            variant="outline"
+            className="bg-white text-silver rounded-xl fixed bottom-10 right-10 py-10 px-7"
+            onClick={handleOpenDialog}
+          >
+            <h1 className="text-xl">
+              Add a<br /> new Task
+            </h1>
           </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
@@ -71,14 +92,52 @@ const Home: React.FC = () => {
         </DialogContent>
       </Dialog>
       <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mx-10 z-[10]">
-        {(checkData && checkData === 404) ? (
+        {checkData && checkData === 404 ? (
           <span>No todos available.</span>
         ) : (
-          todos.map((task: { _id: string; todoName: string; taskId: string }) => (
-            <PostCard key={task._id} todoName={task.todoName} taskId={task._id} reference={ref} />
-          ))
+          todos.map(
+            (task: { _id: string; todoName: string; taskId: string }) => (
+              <PostCard
+                key={task._id}
+                todoName={task.todoName}
+                taskId={task._id}
+                reference={ref}
+              />
+            )
+          )
         )}
       </div>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="rounded-full py-7 bg-[#ffcd10] fixed top-10 right-10">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-lightbulb"
+            >
+              <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+              <path d="M9 18h6" />
+              <path d="M10 22h4" />
+            </svg>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Fun Tip</DialogTitle>
+            <DialogDescription>
+              You can hold and drag the todo cards.<br/> Play with them and have fun :D
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
