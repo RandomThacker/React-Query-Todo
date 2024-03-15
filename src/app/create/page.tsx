1e5b563c35de1751fd97447faa4db1f6848af6ad
RandomThacker <randomthacker@gmail.com>
2024-03-15 18:48:02 +0530

    fix: code issues

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,35 +1,35 @@
-// src/app/create/page.tsx
 "use client"
-import React from "react"; // Import React
 import { createTask } from "@/api/tasks";
+import BackButton from "@/components/BackButton";
 import FormPost from "@/components/FormPost";
 import { FormInputPost } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { SubmitHandler } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
+import React, { PropsWithChildren } from "react";
 
 interface CreatePageProps {
   onCloseDialog: () => void;
 }
 
-const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
-  const router = useRouter();
+const CreatePage: React.FC<PropsWithChildren<CreatePageProps>> = ({ onCloseDialog }) => {
+  const { push } = useRouter();
   const queryClient = useQueryClient();
 
   const createTaskMutation = useMutation({
     mutationFn: createTask,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
-      onCloseDialog();
-    } 
+      onCloseDialog(); // Close the dialog here
+    }
   });
 
   const handleCreateTask: SubmitHandler<FormInputPost> = async (data) => {
     try {
       const newTask = { id: uuidv4(), ...data };
       await createTaskMutation.mutateAsync(newTask);
-      router.push('/');
+      push('/');
     } catch (error) {
       console.error('Error creating task:', error);
     }
@@ -37,6 +37,7 @@ const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
 
   return (
     <div>
+      {/* <BackButton/> */}
       <h1 className="text-2xl my-4 font-bold text-center">Add New Task</h1>
       <FormPost submit={handleCreateTask} isEditing={false}/>
     </div>
