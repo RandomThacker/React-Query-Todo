85ce0af4d5f8703b862280213bc18a12a2987a8e
RandomThacker <randomthacker@gmail.com>
2024-03-15 18:42:37 +0530

    fix

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,37 +1,35 @@
 // src/app/create/page.tsx
 "use client"
+import React from "react"; // Import React
 import { createTask } from "@/api/tasks";
-import BackButton from "@/components/BackButton";
 import FormPost from "@/components/FormPost";
 import { FormInputPost } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from 'next/router'; // Import useRouter from 'next/router'
+import { useRouter } from 'next/router';
 import { SubmitHandler } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
-import { PropsWithChildren } from "react"; // Import PropsWithChildren from 'react'
 
+interface CreatePageProps {
+  onCloseDialog: () => void;
+}
 
-// interface CreatePageProps {
-//   onCloseDialog: () => void;
-// }
-
-const CreatePage: React.FC<any> = ({ onCloseDialog }) => {
-  const router = useRouter(); // Use useRouter from 'next/router' instead of 'next/navigation'
+const CreatePage: React.FC<CreatePageProps> = ({ onCloseDialog }) => {
+  const router = useRouter();
   const queryClient = useQueryClient();
 
   const createTaskMutation = useMutation({
     mutationFn: createTask,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
-      onCloseDialog(); // Close the dialog here
-    }
+      onCloseDialog();
+    } 
   });
 
   const handleCreateTask: SubmitHandler<FormInputPost> = async (data) => {
     try {
       const newTask = { id: uuidv4(), ...data };
       await createTaskMutation.mutateAsync(newTask);
-      router.push('/'); // Use router.push to navigate to the home page
+      router.push('/');
     } catch (error) {
       console.error('Error creating task:', error);
     }
@@ -39,7 +37,6 @@ const CreatePage: React.FC<any> = ({ onCloseDialog }) => {
 
   return (
     <div>
-      {/* <BackButton/> */}
       <h1 className="text-2xl my-4 font-bold text-center">Add New Task</h1>
       <FormPost submit={handleCreateTask} isEditing={false}/>
     </div>
